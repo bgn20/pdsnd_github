182abe6a9fa113eb3504cab4066977344b05391e
Beena <beena.nair@verizon.com>
2021-08-23 22:30:32 -0400

    Refactored checkinput function to evaluate a condition more efficiently

--- bikeshare.py
+++ bikeshare.py
@@ -22,16 +22,16 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    
+
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input("Which city do you want to search in -- chicago, New york city, washington or all?")
-    
+
     # get user input for month (all, january, february, ... , june)
     month = input("Which month do you want to search for?")
-    
+
     # get user input for day of week (all, monday, tuesday, ... sunday)
     day = input("Which day do you want to search for?")
-   
+
     print('-'*40)
     return city.lower(), month.lower(), day.lower()
 
@@ -41,7 +41,7 @@ def check_dfColumns(df):
         if column not in df.columns:
             df[column] = np.nan
     return df
-    
+
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
@@ -53,12 +53,12 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    
+
     if city == "all":
         """read the csv files only from current working directory for loading into dataframe."""
         path = os.getcwd()
         files = os.listdir(path)
-        
+
         files_csv = [f for f in files if f[-3:] == 'csv']
         dfAll = pd.DataFrame(columns = dfColumns)
         for filename in files_csv:
@@ -69,19 +69,19 @@ def load_data(city, month, day):
        filename = CITY_DATA.get(city)
        df = pd.read_csv(filename, index_col=None, header=0, encoding='utf-8-sig', engine='python', skipinitialspace=True)
        df = check_dfColumns(df)
-            
-   
+
+
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['month'] = df['Start Time'].dt.month
     df['day of week'] = df['Start Time'].dt.weekday
     df['hour'] = df['Start Time'].dt.hour
-    
+
     if month != 'all':
         # use the index of the months list to get the corresponding int
         inMonth = months.index(month) + 1
         # filter by month to create the new dataframe
         df = df[df['month'] == inMonth]
-        
+
     # filter by day of week if applicable
     if day != 'all':
         # filter by day of week to create the new dataframe
@@ -97,22 +97,23 @@ def time_stats(df):
 
     # display the most common month
     commonMonth = df['month'].value_counts().idxmax() - 1
-        
+
     print('The most common month is ',str(months[commonMonth]))
 
     # display the most common day of week
-    commonDay = df['day of week'].value_counts().idxmax() 
+    commonDay = df['day of week'].value_counts().idxmax()
     print('The most common day of week is ',str(days[commonDay]))
 
     # display the most common start hour
-    commonHour = df['hour'].value_counts().idxmax() 
-    
+    commonHour = df['hour'].value_counts().idxmax()
+
+
     if(commonHour > 12):
         commonHour -= 12
-        am_or_pm = "PM" 
+        am_or_pm = "PM"
     else:
         am_or_pm = "AM"
-    
+
     print('The most common hour is {} {}'.format( commonHour, am_or_pm))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -151,7 +152,7 @@ def trip_duration_stats(df):
     # display mean travel time
     mean_travel_time = df['Trip Duration'].mean()
     print('The mean time travelled is {}'.format(mean_travel_time))
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -174,34 +175,34 @@ def user_stats(df):
 
     # Display earliest, most recent, and most common year of birth
     earliest_year = df['Birth Year'].min()
-    
+
     if (str(earliest_year) != "nan"):
          print("Earliest birth is in the year {}".format(str(int(df['Birth Year'].min()))))
          print("Most recent birth is in the year {}".format(str(int(df['Birth Year'].max()))))
          print("Most common year of birth is the year {}".format(str(int(df['Birth Year'].value_counts().idxmax()))))
     else:
         print("Data not available for earliest, recent and most common birth year")
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-
-
 
 def check_inputs(city, month, day):
     """Will check for whether all inputs are correct."""
     return check_input("city", city, CITY_DATA) and check_input("month", month, months) and check_input("day", day , days)
-       
+
 
 def check_input(filterName, filterValue, values):
     """Will check for each filtername and its value to ensure input is correct."""
+
+    if (filterValue == "all"):
+        return True
+
     inputGood = False
     for key in values:
-        if filterValue == "all":
-            return True;
         if key == filterValue.lower():
             return True
-    return inputGood        
-  
+    return inputGood
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -220,7 +221,7 @@ def main():
             station_stats(df)
             trip_duration_stats(df)
             user_stats(df)
-    
+
             restart = input('\nWould you like to restart? Enter yes or no.\n')
             if restart.lower() != 'yes':
                 break
@@ -228,7 +229,7 @@ def main():
             restart = input('\nYou have entered the wrong input. Would you like to restart? Enter yes or no.\n')
             if restart.lower() != 'yes':
                 break
-            
+
 
 
 if __name__ == "__main__":
